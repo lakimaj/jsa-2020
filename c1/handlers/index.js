5b01f6cae67b2ea34d0eacf4eff61a7a21564ddc
lakimaj <lazem9@yahoo.com>
2020-06-30 22:20:50 +0200

    Test

--- c1/handlers/index.js
+++ c1/handlers/index.js
@@ -3,7 +3,7 @@ const students = [
     {fname: 'Janko', lname: 'Perovski', gpa: 9.1},
     {fname: 'STanko', lname: 'Perovski', gpa: 7.0},
 ]
-//primer za listna niza objekt
+//primer za listna niza objekt nov
 
 const getAllStudents = (req, res) => {
     return res.status(200).send(students);
